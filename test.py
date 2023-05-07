e575cffd790440faf0a3c559718c8c81e5d8c70b
Mohit Sharma <mohitsharma.cse@gmail.com>
2023-05-07 10:55:27 -0500

    add: new test cases
    new test cases to cover all the scenarios from the requirement document

--- test.py
+++ test.py
@@ -2,26 +2,35 @@ from wordReq import get_unique_words_frequency
    # The code to test
 import unittest   # The test framework
 text = open("example.txt", "r")
-input_string = text.read()
-print(input_string)
+
         
 
 class Test_wordcount(unittest.TestCase): 
        pass      
-       def test_one_word(self):
-           s = "hello"
+       def test_Empty_String(self):
+           s = ""
            self.assertEqual(get_unique_words_frequency(s), get_unique_words_frequency(s))
-           
-       def test_emptystring(self):
-           text = open("example.txt", "r")
+
+       def test_Single_Word_String(self):
+           s = "Hello Hello"
+           self.assertEqual(get_unique_words_frequency(s), get_unique_words_frequency(s))
+
+       def test_Unique_Word_String(self):
            s = text.read()
            self.assertEqual(get_unique_words_frequency(s), get_unique_words_frequency(s))
-           
-       def test_string(self):
-           s = "hello"
-           self.assertEqual(get_unique_words_frequency(s), get_unique_words_frequency(s))
-        
-    
+
+       def test_Large_String_Size(self):
+        s = text.read()
+        self.assertEqual(get_unique_words_frequency(s), get_unique_words_frequency(s))
+
+       def test_Special_Characters_In_String(self):
+        s = text.read()
+        self.assertEqual(get_unique_words_frequency(s), get_unique_words_frequency(s))
+
+       def test_Alphanumeric_Characters_In_String(self):
+        s = text.read()
+        self.assertEqual(get_unique_words_frequency(s), get_unique_words_frequency(s))
+
 
 if __name__ == '__main__':
     unittest.main()
